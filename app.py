fc9f44ac60894dc3179cd235ae6e0de103c66c0e
peet <petrounknown@gmail.com>
2025-01-23 16:05:25 +0300

    updated Source

--- app.py
+++ app.py
@@ -19,23 +19,23 @@ class ChatApp:
         self.buttonAvailabilityFlag=True
 
         sidebarframe=ctk.CTkFrame(root,fg_color="#101010",border_color="#505050",border_width=1,width=200)
-        sidebarframe.grid(row=0,column=0,rowspan=2,sticky="nsew",padx=2,pady=2)
+        sidebarframe.grid(row=0,column=0,rowspan=2,sticky="nsew",padx=(10,5),pady=(10,10))
         self.chatframe=ctk.CTkScrollableFrame(root,fg_color="#101010",border_color="#505050",border_width=1)
-        self.chatframe.grid(row=0,column=1,sticky="nsew",padx=2,pady=2)
+        self.chatframe.grid(row=0,column=1,sticky="nsew",padx=(0,10),pady=(10,0))
         self.chatframe.columnconfigure(0,weight=1)
         self.chatframe.rowconfigure(0,weight=1)
         messageframe=ctk.CTkFrame(root,fg_color="#101010",border_width=0,height=50)
-        messageframe.grid(row=1,column=1,sticky="nsew",padx=2,pady=2)
+        messageframe.grid(row=1,column=1,sticky="nsew",padx=0,pady=0)
         messageframe.columnconfigure(0,weight=1)
         messageframe.rowconfigure(0,weight=1)
         self.constructor_chatContainer("MODEL","Hello there. \nHow can be of assistance today?").grid(row=self.rowCounter,column=0,sticky="nsew",padx=2,pady=2)
         # self.constructor_chatContainer("USER","").grid(row=self.rowCounter,column=0,sticky="nsew",padx=2,pady=2)
         # self.constructor_chatContainer("SYSTEM","SYSTEM ERROR! PLEASE TRY AGAIN.").grid(row=self.rowCounter,column=0,sticky="nsew",padx=2,pady=2)
 
-        textEntry=ctk.CTkEntry(messageframe,border_color="#505050",border_width=1,placeholder_text="Prompt...")
-        textEntry.grid(row=0,column=0,sticky="nsew",padx=2,pady=2)
+        textEntry=ctk.CTkEntry(messageframe,border_color="#505050",border_width=1,placeholder_text="Prompt...",height=40)
+        textEntry.grid(row=0,column=0,sticky="nsew",padx=(0,5),pady=(5,10))
         self.sendButton=ctk.CTkButton(messageframe,text="Send",hover_color="blue",border_color="blue",border_width=1,bg_color="transparent",fg_color="#101010",command=lambda: self.getInput(str(textEntry.get())))
-        self.sendButton.grid(row=0,column=1,sticky="nsew",padx=2,pady=2)
+        self.sendButton.grid(row=0,column=1,sticky="nsew",padx=(0,10),pady=(5,10))
 
 
     def constructor_chatContainer(self,caller,myText):
