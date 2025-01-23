9584c7a1fa987b3587fed2e879e1171f33479598
peet <petrounknown@gmail.com>
2025-01-23 10:41:49 +0300

    update Source

--- app.py
+++ app.py
@@ -1,28 +1,11 @@
-import requests
-import json
+import ollama
 
-url= "http://localhost:11434/api/chat"
+client=ollama.Client()
 
-payload={
-    "model": "mistral",
-    "message": [{
-        "role":"user",
-        "content":"What is Python"
-        }]
-}
+model="sentinel"
+prompt="What is Python?"
 
-response=requests.post(url, json=payload,stream=True)
+response=client.generate(model=model,prompt=prompt)
 
-if response.status_code==200:
-    print("Streaming response from Ollama:")
-    for line in response.iter_lines(decode_unicede=True):
-        if line:
-            try:
-                json_data = json.loads(line)
-                if "message" in json_data and "content" in json_data["message"]:
-                    print(json_data["messafe"]["content"],end="")
-            except json.JSONDecodeError:
-                print(f"\nFailed to parse line: {line}")
-    print()
-else:
-    pass
+print(f"Response froma Ollama model: {model}")
+print(response.response)
